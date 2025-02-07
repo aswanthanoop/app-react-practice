a8913b50a96cecd434b14ff8db387885f195564d
Aswanth Anoop <aswanthanoop42@gmail.com>
2025-02-07 11:42:45 +0530

    Update ListGroup.tsx

--- React jan/react-app/src/components/ListGroup.tsx
+++ React jan/react-app/src/components/ListGroup.tsx
@@ -2,16 +2,14 @@ function ListGroup() {
   let items = ["New York", "San Francisco", "Italy", "Berlin", "Mumbai"];
   items = [];
 
-  if (items.length === 0)
-    return (
-      <>
-        <h1>List</h1>
-        <p>No item found</p>
-      </>
-    );
+  const getMessage = () => {
+    items.length === 0 ? <p>No item</p> : null
+  }
+
   return (
     <>
       <h1>List</h1>
+      {getMessage}
       <ul className="List-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
